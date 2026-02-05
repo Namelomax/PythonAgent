9e79823ce5e57bcf07bf12e248772f9f805e76f0
Jakers <namelomaxer@gmail.com>
2026-02-05 17:17:14 +0800

    Initial commit

---
+++ DocumentAssistant/document_assistant.py
@@ -0,0 +1,14 @@
+class DocumentAssistant:
+    def __init__(
+        self,
+        chunk_size: int = 500,
+        overlap: int = 50,
+        top_k: int = 3
+    ):
+        pass
+
+    def index_documents(self, documents):
+        pass
+
+    def answer_query(self, query: str) -> str:
+        pass
